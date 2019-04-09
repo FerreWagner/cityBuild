7722b54e7483dfe394ce111dc980b019776675c9
FerreWagner <18408229270@163.com>
2019-04-09 23:52:54 +0800

    webdata insert

---
+++ app/admin/controller/Webdata.php
@@ -0,0 +1,27 @@
+<?php
+namespace app\admin\controller;
+
+use app\admin\common\Base;
+use think\Request;
+use app\admin\model\Webdata as WebdataModel;
+class Webdata extends Base
+{
+    
+    /**
+     * 显示资源列表
+     *
+     * @return \think\Response
+     */
+    
+    public function index(Request $request)
+    {
+        //config update
+        if ($request->isPost()) {
+            $data = $request->param();
+            $res  = WebdataModel::update($data, ['id' => 1]);
+            $res ? $this->success('设置成功') : $this->error('更新失败,请稍后重试');
+        }
+        $this->view->assign('webdata', WebdataModel::get(1));
+        return $this->view->fetch('website-data');
+    }
+}
