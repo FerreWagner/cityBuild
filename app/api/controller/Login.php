4a09eef33eada6201528c7ba888a9694aaf29a95
FerreWagner <18408229270@163.com>
2019-04-17 21:47:36 +0800

    add sign info

---
+++ app/api/controller/Login.php
@@ -0,0 +1,40 @@
+<?php
+namespace app\api\controller;
+
+use app\api\Base;
+
+class Login extends Base
+{
+    public function index()
+    {
+        if (request()->isGet()){
+            return json(['code' => '200','msg' => '过分了get老铁']);
+        }elseif(request()->isPost()){
+            return json(['code' => '200','msg' => 'post老铁']);
+        }
+    }
+
+    /**
+     * @return \think\response\Json
+     * 成考列表
+     */
+    public function studyLst()
+    {
+        $data = db('study')->field('major,layer,type,esystem,money')->where(['del' => 0])->order('id', 'asc')->select();
+        if (empty($data)){
+            return json(['code' => '400', 'msg' => '暂无数据']);
+        }
+        return json(['code' => '200', 'data' => $data]);
+    }
+
+    /**
+     * @return \think\response\Json
+     * 网站资料
+     */
+    public function getWebData()
+    {
+        $data = db('webdata')->field('adult_desc,adult_use,adult_where,adult_time,adult_money,adult_major,adult_score,adult_query')->find(1);
+        return json(['code' => '200', 'data' => $data]);
+    }
+    
+}
---
+++ app/api/controller/Sign.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\api\controller;
+
+use app\api\Base;
+use think\Request;
+
+class Sign extends Base
+{
+    public function index()
+    {
+    }
+
+    /**
+     * @param Request $request
+     * @return \think\response\Json
+     * 学生报名
+     */
+    public function signOnWebsite(Request $request)
+    {
+        if ($request->isPost()){
+            $param = $request->param();
+            $res   = db('student')->insert($param);
+            if($res) return json(['code' => '200', 'msg' => '报名成功']);
+            return json(['code' => '99', 'msg' => '报名失败,请认真填写']);
+        }
+    }
+
+}
